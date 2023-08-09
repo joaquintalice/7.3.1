66d42678091b66ced1646ab6ff0918a975d395a3
Joaquin Talice <joaquintalice@gmail.com>
2023-08-09 08:45:48 -0300

    trabajo hecho

--- js/validacion.js
+++ js/validacion.js
@@ -1,3 +1,35 @@
+const pw1 = document.getElementById('password1');
+const pw2 = document.getElementById('password2');
+const checkbox = document.getElementById('terminos');
+const form = document.querySelector('form')
+
+console.log(pw1)
+console.log(pw2)
+console.log(checkbox)
+console.log(form)
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    (validarContraseña() && validarCheckbox()) ? showAlertSuccess() : showAlertError()
+
+});
+
+
+function validarCheckbox() {
+    return checkbox.checked
+}
+
+function validarContraseña() {
+    return pw1.value === pw2.value;
+}
+
+
+
+
+
+
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
 }
